b39a4974394481c26bb10d836788441e2e8ccbc6
abhishek <abhishek.soni1990@gmail.com>
2018-03-16 14:39:17 +0530

    Removed # from Slack Channel in alertmanager & Elastalert

--- elastalert/docker-entrypoint.sh
+++ elastalert/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 cat /opt/rules/elastrules.error.yaml |\
     sed "s@#slack_webhook_url: <url>#@slack_webhook_url: '$SLACK_URL'@g" |\
-    sed "s@#slack_channel_override: <channel>#@slack_channel_override: '#$SLACK_CHANNEL'@g" |\
+    sed "s@#slack_channel_override: <channel>#@slack_channel_override: '$SLACK_CHANNEL'@g" |\
     sed "s@#slack_username_override: <user>#@slack_username_override: '$SLACK_USER'@g" > /tmp/elastrules.error.yaml
 
 mv /tmp/elastrules.error.yaml /opt/rules/elastrules.error.yaml
